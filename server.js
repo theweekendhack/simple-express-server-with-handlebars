2ce0f34b862de647f8e7408c34258ac74978a1ef
kadeembestteaches <56241232+kadeembestteaches@users.noreply.github.com>
2020-07-18 01:23:24 -0400

    Simple Express Server with Handlebars

--- server.js
+++ server.js
@@ -1,125 +1,42 @@
 //import express 
 const express = require("express");
+const exphbs  = require('express-handlebars');
 
 
 //create Express App object 
 const app = express();
 
-//IN order to communicate with a web server created by EXPRESS
-//YOU MUST CREATE ROUTES ()
 
-//TYPES HTTP REQUEST 
-//1. GET
-// 2. POST
-// 3. PUT 
-// 4. DELETE 
-
-//home 
+//This tells the app object that we will be using handlebars as
+//our designated Template engin
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
 
 
-//WET AS F! (CODE DRIVING A TIDA)
-
-
-const htmlTemplate= (data)=>{
-
-    return `<!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>${data.title}</title>
-    </head>
-    <body>
-    <header>
-        <nav>
-            <ul>
-                <li><a href="/">Home</a></li>
-                <li><a href="/about-us">About</a></li>
-                <li><a href="/contact-us">Contact</a></li>
-            </ul>
-        </nav>
-    </header>
-    <section>
-        <h1>${data.headingInfo}</h1>
-    </section>
-    <main>
-        ${data.htmlContent}
-    </main>
-    </body>
-    </html>`;
-}
-
-
-//Routes
 app.get("/",(req,res)=>{
-  
-    
-    const obj = {
+    res.render("index",{
+       title : "Home",
+       data : "The is is dynamic data. YAY"  
 
-        title: 'Home Page',
-        headingInfo: "This is the home page",
-        htmlContent:`<p>This is the HOme blah blah blah blah</p>`
-    }
-
-    res.send(htmlTemplate(obj));
-
-})
-
-//about
-app.get("/about-us",(req,res)=>{
-
-
-    const obj = {
-
-        title: 'About Us Page',
-        headingInfo: "This is the About Us page",
-        htmlContent:`<p>This is the About Page.</p>`
-    }
-
-    res.send(htmlTemplate(obj));
-})
-
-
-//contact-us
-app.get("/contact-us",(req,res)=>{
-
-    const obj = {
-
-        title: 'Home Page',
-        headingInfo: "This is the home page",
-        htmlContent:`<form action="/contact-us"  method="POST">
-
-            <label for="">First Name </label> 
-            <input type="text"> <br><br>
-
-            <label for="">Last Name </label> 
-            <input type="text"> <br><br>
-
-            <label for="">Email Name </label> 
-            <input type="email"> <br><br>
-
-
-            <label for="">Messsage </label> 
-           <textarea rows="5" cols="10"></textarea>
-
-           <input type="submit" value="Register">
-
-        </form>`
-    }
-
-    res.send(htmlTemplate(obj));
-
-
-
- 
-   
+    }); // load index.handlebars (template file)
 });
 
-app.post("/contact-us",(req,res)=>{
-    console.log(`Form was submitted`);
+app.get("/about-us",(req,res)=>{
 
-    res.redirect("/");
+    res.render("aboutUs",{
+        title : "About Us" 
+     })
+});
 
+
+app.get("/contact-us",(req,res)=>{
+    
+   
+    res.render("contactUs",{
+        title : "Contact Us Page",
+        services : ["Phones", "Laptops", "PS5"],
+        images
+     }) 
 });
 
 
